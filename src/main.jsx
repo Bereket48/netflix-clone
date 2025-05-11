b59a04047c9e2733fa2197a74aa2e9bd8f99f4a4
Bereket48 <abatebereket536@gmail.com>
2025-05-11 18:28:31 +0200

    Header and Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css" // Hardcoded thread
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
